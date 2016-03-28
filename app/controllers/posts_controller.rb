507d9ea3ec193c55b8565649bd2a557e3e6ac1a0
yongle <lyuyongle@gmail.com>
2016-03-27 20:38:15 -0500

    article#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
 	def index
-		
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "New Post Created!"
+		else
+			render 'new', notice: "Woops, Something Went Wrong!"
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
 	end
 end
