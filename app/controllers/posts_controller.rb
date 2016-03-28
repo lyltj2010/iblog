6a2bc5c8bae14af126e59236e3338554e42ec44b
yongle <lyuyongle@gmail.com>
2016-03-27 21:07:07 -0500

    article#show and template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
+	end
+
+	def show
 	end
 
 	def new
@@ -20,5 +24,9 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
