f1f36802f8375be754452f747fc08f530ab4e565
yongle <lyuyongle@gmail.com>
2016-04-05 23:46:19 -0500

    custome dashboard layout

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register Post do
 	index do
 		column :title
-		column :content
+		column :content do |post|
+			post.content[0..70]
+		end 
 		column :created_at do |post|
 			div :class => "created_time" do
 				post.created_at.strftime("%B %d %Y")
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -119,7 +119,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can disable the menu item for the comments index page:
   # config.show_comments_in_menu = false
@@ -246,7 +246,7 @@ ActiveAdmin.setup do |config|
   # Pagination is enabled by default for all resources.
   # You can control the default per page count for all resources here.
   #
-  # config.default_per_page = 30
+  config.default_per_page = 30
   #
   # You can control the max per page count too.
   #
