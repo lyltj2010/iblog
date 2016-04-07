d922c4d3f6af64fee4b5bfdf933e0d5a79cf2c77
yongle <lyuyongle@gmail.com>
2016-04-07 10:46:00 -0500

    basic teches CRUD

--- app/controllers/teches_controller.rb
+++ app/controllers/teches_controller.rb
@@ -1,5 +1,6 @@
 class TechesController < ApplicationController
 	before_action :find_tech, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 	def index
 		@teches = Tech.all.order('created_at desc')
 	end
@@ -12,20 +13,34 @@ class TechesController < ApplicationController
 	end
 
 	def create
+		@tech = Tech.new tech_params
+
+		if @tech.save
+			redirect_to @tech, notice: "New Tech Post Created!"
+		else
+			render 'new', notice: "Something Went Wrong"
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		if @tech.update tech_params
+			redirect_to @tech, notice: "Tech Post Updated!"
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@tech.destroy
+		redirect_to teches_path
 	end
 
 	private
 	def find_tech
-		@tech = Post.find(params[:id])	
+		@tech = Tech.find(params[:id])	
 	end
 
 	def tech_params
